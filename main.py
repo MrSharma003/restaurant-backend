8cddac995dca2f0ab2632a8a2108d623fb9a7e5f
MrSharma003 <sprashant145.ms@gmail.com>
2024-12-04 14:57:29 +0530

    added

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ app = FastAPI()
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
 )
 
 # Initialize services
